5c980ccbf0e99b1e2ec40bbdc9abde28b5e011ba
Paco Mendes <paco.mendes@spandigital.com>
2018-01-02 11:57:33 +0200

    Added google analytics

--- media/js/client.js
+++ media/js/client.js
@@ -43,10 +43,6 @@
         scale: 0.3,
         distance: '0px'
     }, 200);
-    sr.reveal('.sr-button', {
-        duration: 1000,
-        delay: 200
-    });
     sr.reveal('.sr-contact', {
         duration: 600,
         scale: 0.3,
